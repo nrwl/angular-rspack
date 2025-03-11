8a419a0c19ac6d2e884eafcaafa354f45bb0facd
Colum Ferry <cferry09@gmail.com>
2025-03-11 15:36:06 +0000

    fix(angular-rspack): dev-server works for ssr (#34)

--- e2e/fixtures/rspack-ssr-css/rspack.config.js
+++ e2e/fixtures/rspack-ssr-css/rspack.config.js
@@ -1,14 +1,29 @@
 module.exports = () => {
   if (global.NX_GRAPH_CREATION === undefined) {
     const { createConfig } = require('@nx/angular-rspack');
-    return createConfig({
-      options: {
-        browser: './src/main.ts',
-        index: './src/index.html',
-        server: './src/main.server.ts',
-        ssr: { entry: './src/server.ts' },
+    return createConfig(
+      {
+        options: {
+          browser: './src/main.ts',
+          index: './src/index.html',
+          server: './src/main.server.ts',
+          ssr: { entry: './src/server.ts' },
+          assets: [
+            {
+              glob: '**/*',
+              input: './public',
+            },
+          ],
+        },
       },
-    });
+      {
+        development: {
+          options: {
+            optimization: false,
+          },
+        },
+      }
+    );
   }
   return {};
 };
