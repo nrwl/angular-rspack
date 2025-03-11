4919981ce1da6fd76a7bfc17ae63dc927a1eb16d
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2025-03-11 14:05:04 +0100

    feat(angular-rspack): support `extractLicenses` option (#30)
    * feat(angular-rspack): support `extractLicenses` option
    
    * chore(angular-rspack): fix pipeline dependencies for unit-test tasks

--- e2e/fixtures/rspack-csr-css/rspack.config.js
+++ e2e/fixtures/rspack-csr-css/rspack.config.js
@@ -2,27 +2,37 @@ module.exports = () => {
   if (global.NX_GRAPH_CREATION === undefined) {
     const { join } = require('path');
     const { createConfig } = require('@nx/angular-rspack');
-    return createConfig({
-      options: {
-        name: 'rspack-csr-css',
-        index: './src/index.html',
-        assets: [{ glob: '**/*', input: 'public' }],
-        styles: ['./src/styles.css'],
-        polyfills: ['zone.js'],
-        main: './src/main.ts',
-        outputPath: {
-          base: './build',
-          browser: './build/static',
-        },
-        outputHashing: 'none',
-        tsConfig: join(__dirname, './tsconfig.app.json'),
-        skipTypeChecking: false,
-        devServer: {
-          port: 8080,
-          proxyConfig: './proxy.conf.json',
+    return createConfig(
+      {
+        options: {
+          name: 'rspack-csr-css',
+          index: './src/index.html',
+          assets: [{ glob: '**/*', input: 'public' }],
+          styles: ['./src/styles.css'],
+          polyfills: ['zone.js'],
+          main: './src/main.ts',
+          outputPath: {
+            base: './build',
+            browser: './build/static',
+          },
+          outputHashing: 'none',
+          tsConfig: join(__dirname, './tsconfig.app.json'),
+          skipTypeChecking: false,
+          devServer: {
+            port: 8080,
+            proxyConfig: './proxy.conf.json',
+          },
         },
       },
-    });
+      {
+        development: {
+          options: {
+            extractLicenses: false,
+            optimization: false,
+          },
+        },
+      }
+    );
   }
   return {};
 };
