16f03218c5eae2acd6377dcdefe764b02a21ab28
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2025-05-05 16:03:47 +0200

    fix(angular-rspack): ensure `stylePreprocessorOptions` is correctly used (#72)

---
+++ e2e/fixtures/rspack-csr-scss/rspack.config.js
@@ -0,0 +1,42 @@
+module.exports = () => {
+  if (global.NX_GRAPH_CREATION === undefined) {
+    const { createConfig } = require('@nx/angular-rspack');
+    return createConfig(
+      {
+        options: {
+          root: __dirname,
+          outputPath: {
+            base: './dist',
+            browser: './browser',
+          },
+          index: './src/index.html',
+          browser: './src/main.ts',
+          tsConfig: './tsconfig.app.json',
+          polyfills: ['zone.js'],
+          assets: [{ glob: '**/*', input: 'public' }],
+          styles: ['./src/styles.scss'],
+          stylePreprocessorOptions: {
+            includePaths: ['../shared/styles/src'],
+          },
+        },
+      },
+      {
+        development: {
+          options: {
+            extractLicenses: false,
+            optimization: false,
+            outputHashing: 'none',
+            namedChunks: true,
+            vendorChunk: true,
+          },
+        },
+        production: {
+          options: {
+            outputHashing: 'all',
+          },
+        },
+      }
+    );
+  }
+  return {};
+};
