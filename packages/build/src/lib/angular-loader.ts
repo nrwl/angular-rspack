601918aaf4f55c63b78db92da93c586918587ebf
Colum Ferry <cferry09@gmail.com>
2025-05-20 12:58:27 +0100

    feat(angular-rspack): match changes in v20

--- packages/build/src/lib/angular-loader.ts
+++
@@ -1,2 +0,0 @@
-import loader from '@nx/angular-rspack/loaders/angular-loader';
-export default loader;
--- packages/build/src/lib/angular-partial-transform-loader.ts
+++
@@ -1,2 +0,0 @@
-import loader from '@nx/angular-rspack/loaders/angular-partial-transform-loader';
-export default loader;
