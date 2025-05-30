19bea19fc0766928be4755b5d1f4d9c51c7962f9
Colum Ferry <cferry09@gmail.com>
2025-05-30 12:35:35 +0200

    feat(angular-rspack,angular-rspack-compiler): support angular 20 (#99)
    * feat(angular-rspack,angular-rspack-compiler): support angular 20
    
    * chore(repo): integration test output updaet
    
    * chore(repo): rsbuild-ssr-csr server entry update
    
    * feat(angular-rspack): match changes in v20
    
    * feat(angular-rspack): update nx and angular versions
    
    ---------
    
    Co-authored-by: Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>

--- e2e/fixtures/rsbuild-ssr-css/src/server.ts
+++ e2e/fixtures/rsbuild-ssr-css/src/server.ts
@@ -1,21 +1,76 @@
-// eslint-disable-next-line @nx/enforce-module-boundaries
-import { createServer } from '@nx/angular-rsbuild/ssr';
+import 'zone.js/node';
+
+import { APP_BASE_HREF } from '@angular/common';
+import { CommonEngine } from '@angular/ssr/node';
+import express from 'express';
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
 import bootstrap from './main.server';
 
-const server = createServer(bootstrap);
+// The Express app is exported so that it can be used by serverless Functions.
+export function app(): express.Express {
+  const server = express();
+  const distFolder = join(process.cwd(), 'dist/browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? join(distFolder, 'index.original.html')
+    : join(distFolder, 'index.html');
 
-/** Add your custom server logic here
- *
- * For example, you can add a custom static file server:
- * server.app.use('/static', express.static(staticFolder));
- * Or add additional api routes:
- * server.app.get('/api/hello', (req, res) => {
- *   res.send('Hello World!');
- * });
- * Or add additional middleware:
- * server.app.use((req, res, next) => {
- *   res.send('Hello World!');
- * });
- */
+  const commonEngine = new CommonEngine();
 
-server.listen();
+  server.set('view engine', 'html');
+  server.set('views', distFolder);
+
+  // Example Express Rest API endpoints
+  // server.get('/api/{*splat}', (req, res) => { });
+  // Serve static files from /browser
+  server.use(
+    express.static(distFolder, {
+      maxAge: '1y',
+      index: false,
+    })
+  );
+
+  // All regular routes use the Angular engine
+  server.use((req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
+
+    commonEngine
+      .render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: distFolder,
+        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+      })
+      .then((html) => res.send(html))
+      .catch((err) => next(err));
+  });
+
+  return server;
+}
+
+function run(): void {
+  const port = process.env['PORT'] || 4000;
+
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    // if (error) {
+    //   throw error;
+    // }
+
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
+
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = (mainModule && mainModule.filename) || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  run();
+}
+
+export default bootstrap;
