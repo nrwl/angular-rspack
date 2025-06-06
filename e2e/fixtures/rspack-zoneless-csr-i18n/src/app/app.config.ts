2f13b4333acae18073d30f9cc5422700bad80e8b
Jakub Chlebowicz <contact@jakubchlebowicz.com>
2025-06-06 17:09:36 +0200

    test: add e2e fixture for zoneless CSR (i18n) with CSS spec

---
+++ e2e/fixtures/rspack-zoneless-csr-i18n/src/app/app.config.ts
@@ -0,0 +1,7 @@
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZonelessChangeDetection(), provideRouter(appRoutes)],
+};
